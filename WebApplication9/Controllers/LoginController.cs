d9d494ffb8b7ff93f818b7932757a8382a1ca09e
shehzadbrohi <shahzadbrohi@hotmail.com>
2017-10-05 10:53:03 -0700

    yes

---
+++ WebApplication9/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+using System.Web.Security;
+
+namespace WebApplication9.Controllers
+{
+    public class LoginController : Controller
+    {
+        TAZEntities db = new TAZEntities();
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(tblUser frmData)
+        {
+            var chk = db.tblUsers.Where(x => x.UserID == frmData.UserID && x.Password == frmData.Password).SingleOrDefault();
+            if (chk != null) 
+            {
+                Session["URL"] = chk.Pic;
+                FormsAuthentication.SetAuthCookie(chk.UserID, false);
+              return  RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+           
+            
+        }
+
+        
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ WebApplication9/Controllers/temsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class temsController : Controller
+    {
+        private TAZEntities db = new TAZEntities();
+
+        // GET: tems
+        public ActionResult Index()
+        {
+            var tblItems = db.tblItems.Include(t => t.tblItemType);
+            return View(tblItems.ToList());
+        }
+
+        // GET: tems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblItem tblItem = db.tblItems.Find(id);
+            if (tblItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblItem);
+        }
+
+        // GET: tems/Create
+        public ActionResult Create()
+        {
+            ViewBag.ItemType = new SelectList(db.tblItemTypes, "ItemType", "ItemType");
+            return View();
+        }
+
+        // POST: tems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,Item,ItemType,Cost")] tblItem tblItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblItems.Add(tblItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ItemType = new SelectList(db.tblItemTypes, "ItemType", "ItemType", tblItem.ItemType);
+            return View(tblItem);
+        }
+
+        // GET: tems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblItem tblItem = db.tblItems.Find(id);
+            if (tblItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ItemType = new SelectList(db.tblItemTypes, "ItemType", "ItemType", tblItem.ItemType);
+            return View(tblItem);
+        }
+
+        // POST: tems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,Item,ItemType,Cost")] tblItem tblItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ItemType = new SelectList(db.tblItemTypes, "ItemType", "ItemType", tblItem.ItemType);
+            return View(tblItem);
+        }
+
+        // GET: tems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblItem tblItem = db.tblItems.Find(id);
+            if (tblItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblItem);
+        }
+
+        // POST: tems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblItem tblItem = db.tblItems.Find(id);
+            db.tblItems.Remove(tblItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
